bee7e39751e56d7ebe3640dbdd8198e7a74df0f2
Zulvan Fahmi <zulvan.fahmi@ibm.com>
2026-02-26 08:43:34 +0700

    first commit

---
+++ src/main/java/com/personal_project/issue_tracking_system/IssueTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.personal_project.issue_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal_project/issue_tracking_system/IssueTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_project.issue_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
